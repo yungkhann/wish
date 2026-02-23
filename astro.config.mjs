feeea5b5cabcbc0ae0dc8a49c8834666e9c8ce70
ieraasyl <95128018+ieraasyl@users.noreply.github.com>
2026-02-23 05:40:00 +0500

    feat: SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://wish.nuacmw.kz",
   srcDir: "frontend",
+
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
@@ -19,6 +23,4 @@ export default defineConfig({
       },
     },
   },
-
-  integrations: [react()],
 });
